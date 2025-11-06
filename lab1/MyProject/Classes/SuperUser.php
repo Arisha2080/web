1f938300aac48afccc284f883c0a628ac2583e87
Arina <gagarinaarina745@yandex.ru>
2025-11-06 13:18:20 +0300

    1

---
+++ lab1/MyProject/Classes/SuperUser.php
@@ -0,0 +1,57 @@
+<?php
+declare(strict_types=1);
+namespace MyProject\Classes;
+
+
+class SuperUser extends User implements SuperUserInterface
+{
+    public $role;
+
+    public static $superUserCount = 0;
+
+    /**
+     * Конструктор класса SuperUser.
+     * 
+     * @param string $name Имя пользователя.
+     * @param string $login Логин пользователя.
+     * @param string $password Пароль пользователя.
+     * @param string $role Роль суперпользователя.
+     */
+    public function __construct($name, $login, $password, $role)
+    {
+        parent::__construct($name, $login, $password);
+        $this->role = $role;
+
+        self::$superUserCount++;
+    }
+
+    /**
+     * Возвращает HTML с информацией о суперпользователе.
+     *
+     * @return string
+     */
+    public function showInfo(): string  
+    {
+        return "<div class=\"super-user-info\">
+                    <h3>Super User Info</h3>
+                    <p><strong>Name:</strong> {$this->name}</p>
+                    <p><strong>Login:</strong> {$this->login}</p>
+                    <p><strong>Role:</strong> {$this->role}</p>
+                </div>";
+    }
+
+
+    /**
+     * Получить информацию о суперпользователе в виде ассоциативного массива
+     * 
+     * @return array Ассоциативный массив с данными пользователя
+     */
+    public function getInfo(): array
+    {
+        return [
+            'name' => $this->name,
+            'login' => $this->login,
+            'role' => $this->role
+        ];
+    }
+}
---
+++ lab1/users.php
@@ -0,0 +1,52 @@
+<?php
+declare(strict_types=1);
+
+use MyProject\Classes\User;
+use MyProject\Classes\SuperUser;
+
+spl_autoload_register(function ($class): void {
+    $file = __DIR__ . '/' . str_replace('\\', '/', $class) . '.php';
+    if (file_exists($file))
+        require $file;
+});
+
+// HTML-заголовок и стили
+echo "<!DOCTYPE html>
+<html lang='ru'>
+<head>
+    <meta charset='UTF-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <title>User Info</title>
+    <link rel=\"stylesheet\" href=\"css/style.css\">
+    
+</head>
+<body>";
+
+$user1 = new User("Виталия", "vit123", "password1");
+$user2 = new User("Андрей", "adrew900", "password2");
+$user3 = new User("Никита", "nik232", "password3");
+
+echo $user1->showInfo();
+echo $user2->showInfo();
+echo $user3->showInfo();
+
+$user = new SuperUser("Admin", "mega_admin", "password4", "administrator");
+echo $user->showInfo();
+
+// Из интерфейса SuperUserInterface:
+print_r($user->getInfo());
+
+
+echo "<div class='stats'>";
+echo "<h3>Статистика:</h3>";
+echo "<p><strong>Всего обычных пользователей:</strong> " . User::$userCount . "</p>";
+echo "<p><strong>Всего супер-пользователей:</strong> " . SuperUser::$superUserCount . "</p>";
+echo "</div>";
+
+unset($user1);
+unset($user2);
+unset($user3);
+unset($user);
+
+
+echo "</body></html>";
